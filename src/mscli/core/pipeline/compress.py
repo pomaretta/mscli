7c0765829c8340045b4ae7d3f17eaa3b62cb91bb
Carlos Pomares <cpomaresp@gmail.com>
2022-06-19 19:06:52 +0200

    Optimized compression

--- src/mscli/core/pipeline/compress.py
+++ src/mscli/core/pipeline/compress.py
@@ -2,6 +2,7 @@ import hashlib
 import logging
 import shutil
 import os
+import tarfile
 
 from ...domain.pipeline.stage import Stage
 from ...domain.builders.builder import MinecraftBuilder
@@ -9,6 +10,15 @@ from ...shared.files import remove_directory, create_directories
 from datetime import datetime
 
 class CompressDirectory(Stage):
+
+    exclude_dirs = [
+        "logs", # NOTE: Not include logs in the compressed file
+        "libraries", # NOTE: Not include libraries
+        "versions", # NOTE: Not include versions in the compressed file
+    ]
+    exclude_files = [
+        ".DS_Store" # NOTE: This is a MacOS file
+    ]
 
     def __init__(self,builder:  MinecraftBuilder, stage_id: str, name: str, description: str, directory_path: list, output_path: str, output: list = None):
         super().__init__(builder, stage_id, name, description)
@@ -39,9 +49,21 @@ class CompressDirectory(Stage):
             f"build_{self.builder.provider.name}-{self.builder.provider.version}_{yy}-{mm}-{dd}-{HH}-{MM}-{ss}"
         )
 
-        shutil.make_archive(output_compressed, 'zip', directory)
+        # TODO: Use tarfile to compress the directory
+        tar = tarfile.open(output_compressed + ".tar.gz", "w:gz")
+        for root, dirs, files in os.walk(directory, topdown=True):
+            dirs[:] = [d for d in dirs if d not in self.exclude_dirs]
+            files[:] = [f for f in files if f not in self.exclude_files]
+            for file in files:
+                with open(os.path.join(root, file), "rb") as f:
+                    # NOTE: Add the file to the tarfile
+                    tar.addfile(
+                        tarinfo=tarfile.TarInfo(os.path.join(root, file)),
+                        fileobj=f.read()
+                    )
+        tar.close()
 
-        if not os.path.exists(output_compressed + ".zip"):
+        if not os.path.exists(output_compressed + ".tar.gz"):
             logging.error("Failed to create compressed file")
             self._failed = True
             self._completed = True
@@ -50,15 +72,15 @@ class CompressDirectory(Stage):
         # Calculate checksum
         checksum = None
 
-        with open(output_compressed + ".zip", 'rb') as f:
+        with open(output_compressed + ".tar.gz", 'rb') as f:
             data = f.read()
             checksum = hashlib.md5(data).hexdigest()
             f.close()
 
-        self._output.append(output_compressed + ".zip")
+        self._output.append(output_compressed + ".tar.gz")
         self._output.append(checksum)
         if self.output is not None:
-            self.output.append(output_compressed + ".zip")
+            self.output.append(output_compressed + ".tar.gz")
             self.output.append(checksum)
         self._completed = True
         return True
@@ -87,17 +109,16 @@ class UncompressDirectory(Stage):
             self._completed = True
             return False
 
-        shutil.unpack_archive(
-            compressed_path,
-            self.output_path,
-            format='zip'
-        )
-
-        if not os.path.exists(self.output_path):
-            logging.error("Failed to uncompress file")
-            self._failed = True
-            self._completed = True
-            return False
+        # shutil.unpack_archive(
+        #     compressed_path,
+        #     self.output_path,
+        #     format='zip'
+        # )
+        # if not os.path.exists(self.output_path):
+        #     logging.error("Failed to uncompress file")
+        #     self._failed = True
+        #     self._completed = True
+        #     return False
 
         # Remove old data
         remove_directory(self.output_path)
@@ -108,7 +129,7 @@ class UncompressDirectory(Stage):
         shutil.unpack_archive(
             compressed_path,
             self.output_path,
-            format='zip'
+            format='gztar'
         )
 
         if not os.path.exists(self.output_path):
